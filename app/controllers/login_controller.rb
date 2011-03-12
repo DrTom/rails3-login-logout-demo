c86420e527e78d51eee11180479bba43273a5016
Thomas Schank <DrTom@schank.ch>
2011-03-12 15:14:24 +0100

    set up user-model with autenticate functionality and _create_ method in LoginController

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,9 +2,16 @@
 class LoginController < ApplicationController
 
   def new
+    
   end
 
   def create
+    if user = User.authenticate(params[:name],params[:password])
+      session[:user_id] = user.id
+      redirect_to users_url, :notice => 'login succeed'
+    else
+      redirect_to login_url, :alert => 'login failed'
+    end
   end
 
   def delete
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+
+  def User.authenticate name, password
+    if user = User.find_by_name(name)
+      if password == user.password 
+        user
+      end
+    end
+  end
+
 end
+
